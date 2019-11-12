0058737803a886ed0e140994c396745e16555974
Bogdan Livadaru <bogdanlivadaru@yahoo.com>
2019-11-12 14:54:12 -0800

    Class Demo Help

---
+++ Module8/MyConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In class demo
+            if (args.Length == 0) Console.WriteLine("You do not have any arguments");
+            else
+            {
+                foreach(string s in args)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            //Console.WriteLine(args.Length);
+        }
+    }
+}
---
+++ Module8/WinServicePraf/WinService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinServicePraf
+{
+    public partial class WinService : ServiceBase
+    {
+        public WinService()
+        {
+            InitializeComponent();
+            if (!EventLog.SourceExists("WinService"))
+            {
+                EventLog.CreateEventSource("WinService", "Application");
+            }
+            eventLog1.Source = "WinService";
+            eventLog1.Log = "Application";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("Starting the service", EventLogEntryType.Information, 1001);
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("Stopping the service", EventLogEntryType.Information, 1001);
+        }
+    }
+}
