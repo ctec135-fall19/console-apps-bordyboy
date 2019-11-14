69fe2bbfa960efbfbaa9b29ea1544932683761df
Bogdan Livadaru <bogdanlivadaru@yahoo.com>
2019-11-13 20:13:36 -0800

    Almost all done, some errors.

---
+++ Module8/CA2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the CA2 program");
+            string input = Console.ReadLine();
+            Console.WriteLine(input);
+        }
+    }
+}
